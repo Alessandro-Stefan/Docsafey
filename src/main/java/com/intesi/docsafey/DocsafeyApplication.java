4fd82cb8e58e9630223fea8e3c36d6154bfd034c
alessandro-stefan <alessandrostefan047@gmail.com>
2026-07-16 01:37:33 +0200

    Creazione struttura base progetto

---
+++ src/main/java/com/intesi/docsafey/DocsafeyApplication.java
@@ -0,0 +1,13 @@
+package com.intesi.docsafey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsafeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsafeyApplication.class, args);
+	}
+
+}
